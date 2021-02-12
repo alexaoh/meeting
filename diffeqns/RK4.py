de9e4284b0e2f43e952baebbd43ea41774d0b17c
alexaoh <alexaoh@users.noreply.github.com>
2021-02-12 16:44:30 +0100

    akjfdh akfjh vscode.
    akjfdh akfjh vscode.

--- diffeqns/RK4.py
+++ diffeqns/RK4.py
@@ -5,6 +5,7 @@ def kutta4_step(f, t, y, h):
     f2 = f(t+(h/2), y+(h/2)*f1)
     f3 = f(t+(h/2), y+(h/2)*f2)
     f4 = f(t+h, y+h*f3)
+    # Dette er min endring.
     return y + (h/6)*(f1+2*f2+2*f3+f4)
 
     
@@ -13,9 +14,10 @@ def kutta4(f,t0,y0,h,nsteps):
     m = len(y0)
     Y = np.zeros((nsteps+1,m))
     T = np.zeros(nsteps+1) # heisann
-    T[0] = t0
+    T[0] = t0adstgføkj ag ølaks dfaø            
     Y[0] = y0
     for i in range(nsteps):
         Y[i+1] = kutta4_step(f, T[i], Y[i], h)
         T[i+1] = T[i] + h
     return T,Y
+
